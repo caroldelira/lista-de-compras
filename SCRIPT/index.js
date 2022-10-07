f866945c2ac9c50425942ba9d5006fcc9ed10ea6
Carol de Lira <caroldelira@hotmail.com>
2022-10-06 23:04:05 -0300

    Inclusão de Leitura dos Dados

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -1,17 +1,56 @@
 class Produto{
 
     constructor(){
-        this.nome = '';
-        this.valor = 0;
-        this.quantidade = 0;
+        this.id = 1;
+        this.lista = [];
     }
 
     adicionar(){
-        alert('Produto Adicionado');
+        let listaCompras = this.lerDados();
+
+        if(this.validaCampos(listaCompras)){
+            this.salvar(this.lista);
+        };
+
+        console.log(listaCompras);
+        
     }
 
-    excluir(){
-        alert('Produto Excluido')
+    lerDados(){
+        let listaCompras = [];
+
+        listaCompras.idProdutos = this.id;
+        listaCompras.nome = document.getElementById("idProdutos").value;
+        listaCompras.valor = document.getElementById("idValor").value;
+        listaCompras.qtd = document.getElementById("idQtd").value;
+
+        return listaCompras;
+    }
+
+    salvar(listaCompras){
+        this.lista.push(listaCompras);
+        this.id++;
+    }
+
+    validaCampos(listaCompras){
+        let msg = '';
+
+        if(listaCompras.nome == ''){
+            msg += ' Informe o nome do Produto \n'
+        }
+        if(listaCompras.valor == ''){
+            msg += ' Informe o valor do produto \n'
+        }
+        if(msg != ''){
+            alert(msg);
+            return false;
+        }
+
+        return true;
+    }
+
+    cancelar(){
+        alert('Produto Excluido');
     }
 }
 
